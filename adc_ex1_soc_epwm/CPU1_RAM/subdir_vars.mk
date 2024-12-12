46a068a76e3c20f6afdb592873377814a18c805c
nirdhesh58 <Nirdhesh.Krishna@adityaavartan.com>
2024-12-12 17:21:24 +0530

    updated
    not tested yret

---
+++ adc_ex1_soc_epwm/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,110 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../280013x_generic_ram_lnk.cmd \
+../f280013x_headers_nonBIOS.cmd 
+
+ASM_SRCS += \
+../f280013x_codestartbranch.asm \
+../f280013x_usdelay.asm 
+
+C_SRCS += \
+../adc_ex1_soc_epwm.c \
+../f280013x_adc.c \
+../f280013x_cputimers.c \
+../f280013x_defaultisr.c \
+../f280013x_epwm.c \
+../f280013x_globalvariabledefs.c \
+../f280013x_gpio.c \
+../f280013x_piectrl.c \
+../f280013x_pievect.c \
+../f280013x_sysctrl.c \
+../f280013x_tempsensorconv.c 
+
+C_DEPS += \
+./adc_ex1_soc_epwm.d \
+./f280013x_adc.d \
+./f280013x_cputimers.d \
+./f280013x_defaultisr.d \
+./f280013x_epwm.d \
+./f280013x_globalvariabledefs.d \
+./f280013x_gpio.d \
+./f280013x_piectrl.d \
+./f280013x_pievect.d \
+./f280013x_sysctrl.d \
+./f280013x_tempsensorconv.d 
+
+OBJS += \
+./adc_ex1_soc_epwm.obj \
+./f280013x_adc.obj \
+./f280013x_codestartbranch.obj \
+./f280013x_cputimers.obj \
+./f280013x_defaultisr.obj \
+./f280013x_epwm.obj \
+./f280013x_globalvariabledefs.obj \
+./f280013x_gpio.obj \
+./f280013x_piectrl.obj \
+./f280013x_pievect.obj \
+./f280013x_sysctrl.obj \
+./f280013x_tempsensorconv.obj \
+./f280013x_usdelay.obj 
+
+ASM_DEPS += \
+./f280013x_codestartbranch.d \
+./f280013x_usdelay.d 
+
+OBJS__QUOTED += \
+"adc_ex1_soc_epwm.obj" \
+"f280013x_adc.obj" \
+"f280013x_codestartbranch.obj" \
+"f280013x_cputimers.obj" \
+"f280013x_defaultisr.obj" \
+"f280013x_epwm.obj" \
+"f280013x_globalvariabledefs.obj" \
+"f280013x_gpio.obj" \
+"f280013x_piectrl.obj" \
+"f280013x_pievect.obj" \
+"f280013x_sysctrl.obj" \
+"f280013x_tempsensorconv.obj" \
+"f280013x_usdelay.obj" 
+
+C_DEPS__QUOTED += \
+"adc_ex1_soc_epwm.d" \
+"f280013x_adc.d" \
+"f280013x_cputimers.d" \
+"f280013x_defaultisr.d" \
+"f280013x_epwm.d" \
+"f280013x_globalvariabledefs.d" \
+"f280013x_gpio.d" \
+"f280013x_piectrl.d" \
+"f280013x_pievect.d" \
+"f280013x_sysctrl.d" \
+"f280013x_tempsensorconv.d" 
+
+ASM_DEPS__QUOTED += \
+"f280013x_codestartbranch.d" \
+"f280013x_usdelay.d" 
+
+C_SRCS__QUOTED += \
+"../adc_ex1_soc_epwm.c" \
+"../f280013x_adc.c" \
+"../f280013x_cputimers.c" \
+"../f280013x_defaultisr.c" \
+"../f280013x_epwm.c" \
+"../f280013x_globalvariabledefs.c" \
+"../f280013x_gpio.c" \
+"../f280013x_piectrl.c" \
+"../f280013x_pievect.c" \
+"../f280013x_sysctrl.c" \
+"../f280013x_tempsensorconv.c" 
+
+ASM_SRCS__QUOTED += \
+"../f280013x_codestartbranch.asm" \
+"../f280013x_usdelay.asm" 
+
+
